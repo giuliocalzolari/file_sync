e633aa9dda616f017284546662467ec01fc088ad
Giulio Calzolari <giulio.calzolari@cloudreach.co.uk>
2015-05-22 10:20:26 +0200

    Update scheduled uselss class

--- file_sync.py
+++ file_sync.py
@@ -18,39 +18,6 @@ from threading import Timer
 __author__ = ['Giulio.Calzolari']
 
 
-class RepeatedTimer(object):
-
-    def __init__(self, interval, function, *args, **kwargs):
-        self._timer     = None
-
-        if interval < 10:
-            self.interval   = 10
-        else:
-            self.interval   = interval
-
-
-        self.function   = function   
-        self.args       = args
-        self.kwargs     = kwargs
-        self.is_running = False
-        self.start()
-
-    def _run(self):
-        self.is_running = False
-        self.start()
-        self.function(*self.args, **self.kwargs)
-
-    def start(self):
-        if not self.is_running:
-            self._timer = Timer(self.interval, self._run)
-            self._timer.start()
-            self.is_running = True
-
-    def stop(self):
-        self._timer.cancel()
-        self.is_running = False
-
-
 class ChangeHandler(FileSystemEventHandler):
 
     def __init__(self, config, ec2, log):
